bfde25643fd7b82ec90085900768f947d5f9ca46
Rohith Thammaiah <rohit@iciindia.in>
2019-12-04 21:15:58 +0530

    added necessary modules

--- app/src/main/java/me/assignment/playo/newsapp/di/PlayoNewsAppComponent.kt
+++ app/src/main/java/me/assignment/playo/newsapp/di/PlayoNewsAppComponent.kt
@@ -1,9 +1,18 @@
 package me.assignment.playo.newsapp.di
 
 import dagger.Component
+import me.assignment.playo.newsapp.di.modules.ApiModule
+import me.assignment.playo.newsapp.di.modules.BaseUrlModule
+import me.assignment.playo.newsapp.di.modules.ContextModule
+import me.assignment.playo.newsapp.di.modules.RxSchedulerModule
 import me.assignment.playo.newsapp.di.scopes.ApplicationScope
 
 @ApplicationScope
-@Component(modules = [])
+@Component(modules = [
+    ContextModule::class,
+    ApiModule::class,
+    RxSchedulerModule::class,
+    BaseUrlModule::class
+])
 interface PlayoNewsAppComponent {
 }
