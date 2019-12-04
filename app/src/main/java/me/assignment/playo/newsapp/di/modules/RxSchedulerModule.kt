10987acbbe0835d48db28e7464f302b9e95b0eb0
Rohith Thammaiah <rohit@iciindia.in>
2019-12-04 21:38:19 +0530

    removed unnecessary named qualifier

--- app/src/main/java/me/assignment/playo/newsapp/di/modules/RxSchedulerModule.kt
+++ app/src/main/java/me/assignment/playo/newsapp/di/modules/RxSchedulerModule.kt
@@ -2,17 +2,15 @@ package me.assignment.playo.newsapp.di.modules
 
 import dagger.Module
 import dagger.Provides
+import me.assignment.playo.newsapp.di.scopes.ApplicationScope
 import me.assignment.playo.newsapp.rx.AppSchedulerProvider
 import me.assignment.playo.newsapp.rx.SchedulerProvider
-import me.assignment.playo.newsapp.di.scopes.ApplicationScope
-import javax.inject.Named
 
 @Module
 class RxSchedulerModule {
 
     @Provides
     @ApplicationScope
-    @Named("scheduler")
     fun provideScheduler(): SchedulerProvider {
         return AppSchedulerProvider()
     }
