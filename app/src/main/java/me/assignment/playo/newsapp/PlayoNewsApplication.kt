6d581028bfdfcb77d89fe128045ba57539a3377a
Rohith Thammaiah <rohit@iciindia.in>
2019-12-04 21:40:37 +0530

    initializing dagger component

--- app/src/main/java/me/assignment/playo/newsapp/PlayoNewsApplication.kt
+++ app/src/main/java/me/assignment/playo/newsapp/PlayoNewsApplication.kt
@@ -2,12 +2,21 @@ package me.assignment.playo.newsapp
 
 import android.app.Application
 import com.facebook.stetho.Stetho
+import me.assignment.playo.newsapp.di.DaggerPlayoNewsAppComponent
+import me.assignment.playo.newsapp.di.PlayoNewsAppComponent
+import me.assignment.playo.newsapp.di.modules.ContextModule
 import timber.log.Timber
 
 class PlayoNewsApplication : Application() {
 
+    lateinit var appComponent : PlayoNewsAppComponent
+
     override fun onCreate() {
         super.onCreate()
+
+        appComponent = DaggerPlayoNewsAppComponent.builder()
+            .contextModule(ContextModule(this))
+            .build()
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
