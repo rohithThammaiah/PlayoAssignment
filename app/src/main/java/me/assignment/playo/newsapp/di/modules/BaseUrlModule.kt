1102f6c58f7a74e7dc1f2822679ee704b5fc3484
Rohith Thammaiah <rohit@iciindia.in>
2019-12-04 20:27:06 +0530

    added base url for hacker news

---
+++ app/src/main/java/me/assignment/playo/newsapp/di/modules/BaseUrlModule.kt
@@ -0,0 +1,17 @@
+package me.assignment.playo.newsapp.di.modules
+
+import dagger.Module
+import dagger.Provides
+import me.assignment.playo.newsapp.di.scopes.ApplicationScope
+import javax.inject.Named
+
+@Module
+class BaseUrlModule {
+
+    @Provides
+    @ApplicationScope
+    @Named("baseUrl")
+    fun provideBaseUrl(): String {
+        return "https://hn.algolia.com/api/v1/"
+    }
+}
