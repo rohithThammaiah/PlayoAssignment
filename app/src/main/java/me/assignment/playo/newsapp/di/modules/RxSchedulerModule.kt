020feec665fb64ce87787c05562255f38bcd4ad9
Rohith Thammaiah <rohit@iciindia.in>
2019-12-04 21:16:44 +0530

    moved file out of di

--- app/src/main/java/me/assignment/playo/newsapp/di/modules/RxSchedulerModule.kt
+++ app/src/main/java/me/assignment/playo/newsapp/di/modules/RxSchedulerModule.kt
@@ -2,8 +2,8 @@ package me.assignment.playo.newsapp.di.modules
 
 import dagger.Module
 import dagger.Provides
-import me.assignment.playo.newsapp.di.rx.AppSchedulerProvider
-import me.assignment.playo.newsapp.di.rx.SchedulerProvider
+import me.assignment.playo.newsapp.rx.AppSchedulerProvider
+import me.assignment.playo.newsapp.rx.SchedulerProvider
 import me.assignment.playo.newsapp.di.scopes.ApplicationScope
 import javax.inject.Named
 
