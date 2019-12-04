68a81b3493ae699d57fe29ee25dfa9eb40bbfa5c
Rohith Thammaiah <rohit@iciindia.in>
2019-12-04 20:39:19 +0530

    home screen init

---
+++ app/src/main/java/me/assignment/playo/newsapp/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package me.assignment.playo.newsapp.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.assignment.playo.newsapp.R
+
+class HomeScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_screen)
+    }
+}
